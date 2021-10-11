afbb012582737307d17e39aab8d02aab6b3479c6
Nick Hennessy <njhnny@gmail.com>
2021-10-10 19:39:25 -0700

    add clients to stylist model

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -7,6 +7,7 @@ namespace HairSalon.Models
     public int StylistId { get; set; }
     public string Name { get; set; }
     public string Details { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
 
   }
 }
