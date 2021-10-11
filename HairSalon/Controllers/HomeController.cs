6c95e4b7ee655165543c8216af317f40862f69b8
Nick Hennessy <njhnny@gmail.com>
2021-10-10 18:17:09 -0700

    update HomeController to use index

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -4,10 +4,10 @@ namespace HairSalon.Controllers
 {
   public class HomeController : Controller
   {
-
-    [Route("/hello")]
-    public string Hello() { return "Hello friend!"; }
-    [Route("/goodbye")]
-    public string Goodbye() { return "Goodbye friend!"; }
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
   }
 }
